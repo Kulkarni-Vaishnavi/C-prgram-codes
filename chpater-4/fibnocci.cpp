36f146d900368a5beba765fd7631f47c4378ab22
harshak02 <shkancharla7777@gmail.com>
2022-01-08 23:26:02 +0530

    second_commit_by_shk

--- chpater-4/fibnocci.cpp
+++ chpater-4/fibnocci.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int fib(int n){
-    if(n<2){
+    if(n==2){
         return 1;
     }
+
+    else if(n==1){
+        return 1;
+    }
+
     return fib(n-2) + fib(n-1);
 }
 
